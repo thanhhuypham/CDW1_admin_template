2c8a92c408be085395f948839ff53b4ecb873966
thanhhuypham <thanhhuy.25101996@gmail.com>
2017-04-02 20:19:10 +0700

    Add module 23 and Update module 22, date: 02/04/2017

--- 22/type-22.php
+++ 22/type-22.php
@@ -9,6 +9,7 @@
 
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
@@ -19,6 +20,8 @@
         <link href="css/type-22.css" rel="stylesheet" type="text/css" />
         <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/jquery.dataTables.min.js" type="text/javascript"></script>
+        <script src="js/dataTables.bootstrap.min.js" type="text/javascript"></script>
         <script src="js/custom-script.js" type="text/javascript"></script>
 
     </head>
